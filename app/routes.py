02830f1e133699018630d125457318692cf5a2c4
Mica <migatsar@gmail.com>
2020-10-20 17:32:15 +0200

    Simplification of the project structure

--- app/routes.py
+++ app/routes.py
@@ -7,39 +7,3 @@ from app import app
 @app.route('/index')
 def index():
     return render_template('index.html')
-
-
-"""
-from flask import render_template, flash, redirect
-from app.forms import LoginForm
-"""
-
-"""
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'username': 'Mica'}
-    posts = [
-        {
-            'author': {'username': 'Joe'},
-            'body': 'Beautiful day in Portland!'
-        }, 
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
-"""
-
-"""
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data
-            ))
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
-"""
