a4632552aece6f9347261411242dbd0b59f9e7c2
Mica <migatsar@gmail.com>
2020-11-11 21:35:11 +0100

    Refactoring of filenames to debug the deployment on Heroku

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 from app import app
+import logging
 
 if __name__ == "__main__":
+
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
     app.run(debug=True)
