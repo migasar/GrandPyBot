82af066eed4958f6e391bd4c7015344a6ea56252
Mica <migatsar@gmail.com>
2020-11-17 03:46:15 +0100

    "Added rules to the layout of the grid"

--- app/static/index.js
+++ app/static/index.js
@@ -3,15 +3,14 @@ let userForm = document.querySelector('#form-container');
 let userText = document.querySelector('#userText');
 
 
-function addDialogElt(contentElt, classElt) {
+function addDialogElt(containerElt, contentElt, classElt) {
   /* Add an element to the dialog segment, on the web page. */
 
   let dialogElt = document.createElement('div');
   dialogElt.className = classElt;
   dialogElt.textContent = contentElt;
-  dialogBox.appendChild(dialogElt);
+  containerElt.appendChild(dialogElt);
 };
-
 
 function addMapElt(answerMap) {
   /* Add a map object to the dialog segment, on the web page. */
@@ -20,7 +19,6 @@ function addMapElt(answerMap) {
   botMap.className = 'map';
   dialogBox.appendChild(botMap);
   initMap(answerMap['latitude'], answerMap['longitude'], botMap)
-
 };
 
 function initMap(placeLatitude, placeLongitude, div) {
@@ -44,7 +42,7 @@ function initMap(placeLatitude, placeLongitude, div) {
   });
 };
 
-function composeAnswer(answerData) {
+function composeAnswer(containerElt, answerData) {
   /*  
    * Agregate the components of the whole reply from the bot, 
    * depending of the result found to the user question. 
@@ -52,16 +50,16 @@ function composeAnswer(answerData) {
 
   if (answerData['spotted'] == true) {
     // Add text
-    addDialogElt(answerData['context'], 'answer');
-    addDialogElt(answerData['extract'], 'answer');
+    addDialogElt(containerElt, answerData['context'], 'answer');
+    addDialogElt(containerElt, answerData['extract'], 'answer');
     // Add the address of the location
-    addDialogElt(`Voici où ton chemin te mène :   ${answerData['address']}`, 'answer');
+    addDialogElt(containerElt , `Voici où ton chemin te mène :   ${answerData['address']}`, 'answer');
     // Add a map of the location
     addMapElt(answerData);
   } else {
     // Add text
-    addDialogElt(answerData['context'], 'answer');
-    addDialogElt(answerData['reply'], 'answer');
+    addDialogElt(containerElt, answerData['context'], 'answer');
+    addDialogElt(containerElt, answerData['reply'], 'answer');
   }
 };
 
@@ -90,17 +88,133 @@ userForm.addEventListener('submit', function (event) {
   // Block the refreshment of the page
   event.preventDefault();
 
+
+  // Add an element to regroup the items
+  let exchangeContainer = document.createElement('div');
+  exchangeContainer.className = 'exchange';
+  dialogBox.appendChild(exchangeContainer);
+
+
   // Add the question of the user to the main dialog
-  addDialogElt(userText.value, 'question');
+  addDialogElt(exchangeContainer, userText.value, 'question');
 
   // Send the content of the input to the server
   postFormData("/ask", new FormData(userForm))
     .then(data => {
       // Call a function to create the answer of the bot
-      composeAnswer(data)
+      composeAnswer(exchangeContainer, data)
     })
     .catch(error => console.log(error));
 
   // Clean the input field after settling the user request
   resetInputForm(userForm);
 });
+
+
+/* # ########## # */
+// let dialogBox = document.querySelector('#dialog-container');
+// let userForm = document.querySelector('#form-container');
+// let userText = document.querySelector('#userText');
+
+
+// function addDialogElt(contentElt, classElt) {
+//   /* Add an element to the dialog segment, on the web page. */
+
+//   let dialogElt = document.createElement('div');
+//   dialogElt.className = classElt;
+//   dialogElt.textContent = contentElt;
+//   dialogBox.appendChild(dialogElt);
+// };
+
+// function addMapElt(answerMap) {
+//   /* Add a map object to the dialog segment, on the web page. */
+
+//   let botMap = document.createElement('div');
+//   botMap.className = 'map';
+//   dialogBox.appendChild(botMap);
+//   initMap(answerMap['latitude'], answerMap['longitude'], botMap)
+// };
+
+// function initMap(placeLatitude, placeLongitude, div) {
+//   /* Initialize a map object. */
+
+//   // Define the location by its coordinates
+//   let place = {
+//     lat: placeLatitude,
+//     lng: placeLongitude
+//   };
+//   // Initialize the map, centered on the location
+//   let map = new google.maps.Map(div, {
+//     zoom: 15,
+//     center: place
+//   });
+
+//   // Add a marker to the map, positioned at the location
+//   let marker = new google.maps.Marker({
+//     position: place,
+//     map: map
+//   });
+// };
+
+// function composeAnswer(answerData) {
+//   /*  
+//    * Agregate the components of the whole reply from the bot, 
+//    * depending of the result found to the user question. 
+//    */
+
+//   if (answerData['spotted'] == true) {
+//     // Add text
+//     addDialogElt(answerData['context'], 'answer');
+//     addDialogElt(answerData['extract'], 'answer');
+//     // Add the address of the location
+//     addDialogElt(`Voici où ton chemin te mène :   ${answerData['address']}`, 'answer');
+//     // Add a map of the location
+//     addMapElt(answerData);
+//   } else {
+//     // Add text
+//     addDialogElt(answerData['context'], 'answer');
+//     addDialogElt(answerData['reply'], 'answer');
+//   }
+// };
+
+// function postFormData(url, data) {
+//   /* Async function to send the content of the input to the server */
+
+//   return fetch(url, {
+//       method: "POST",
+//       body: data
+//     })
+//     .then(response => response.json())
+//     .catch(error => console.log(error));
+// };
+
+// function resetInputForm(docElt) {
+//   /* Remove the user request from the input field */
+//   docElt.reset();
+// }
+
+// userForm.addEventListener('submit', function (event) {
+//   /* 
+//    * Catch the content of the form sent by the user,
+//    * search for the elements of a response, and bring them back. 
+//    */
+
+//   // Block the refreshment of the page
+//   event.preventDefault();
+
+//   // Add the question of the user to the main dialog
+//   addDialogElt(userText.value, 'question');
+
+//   // Send the content of the input to the server
+//   postFormData("/ask", new FormData(userForm))
+//     .then(data => {
+//       // Call a function to create the answer of the bot
+//       composeAnswer(data)
+//     })
+//     .catch(error => console.log(error));
+
+//   // Clean the input field after settling the user request
+//   resetInputForm(userForm);
+// });
+
+
